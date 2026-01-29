22d6376a665eeef26840391f2699d9fa57b8ae3c
Bihari Mali <bihari.mali2023@ssipmt.com>
2026-01-29 21:44:38 +0530

    Add files via upload

---
+++ reference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Reference
+    int a=10;
+    int &r=a;
+    cout<<r<<endl;
+
+    // // Assignment
+    // int b=30;
+    // int c=b;
+    // cout<<c<<endl;
+
+    return 0;
+}

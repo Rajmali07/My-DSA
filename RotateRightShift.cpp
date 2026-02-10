c1f9c81f6da65e916b69c734fef4bc21bae9304c
Bihari Mali <bihari.mali2023@ssipmt.com>
2026-02-10 22:21:32 +0530

    Add files via upload

---
+++ RotateRightShift.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]= {5,6,8,2,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int temp = arr[n-1];
+    for(int i=n-1; i>0; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0]=temp;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}

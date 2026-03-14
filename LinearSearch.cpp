49cbfa0500224469993c8212aa8bf87e1331ebdc
Bihari Mali <bihari.mali2023@ssipmt.com>
2026-03-14 16:50:15 +0530

    linear searh and power

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,17 +1,36 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-int SearchTarget(vector<int> vec, int target){
-    for (int i = 0; i < 5; i++)
+struct Array{
+    int A[20];
+    int size;
+    int length;
+};
+void Display(struct Array arr){
+    int i;
+    cout<<"Elements are : ";
+    for (int i = 0; i < arr.length; i++)
     {
-        if(vec[i]==target){
+        cout<<arr.A[i]<<" ";
+    }
+}
+int linear1(struct Array arr, int x){
+    for (int i = 0; i < arr.length; i++)
+    {
+        if(x==arr.A[i]){
             return i;
         }
     }
     return -1;
+    
 }
 int main(){
-    vector<int> vec={1,2,3,4,5,6};
-    cout<<SearchTarget(vec, 5);
+    struct Array arr={{2,3,5,9,7},20,5};
+    int index=linear1(arr,5);
+    if(index != -1)
+        cout << "Element found at index " << index << endl;
+    else
+        cout << "Element not found" << endl;
+
+    Display(arr);
     return 0;
 }
--- Power.cpp
+++ Power.cpp
@@ -1,43 +1,33 @@
-// By converting power into binary, O(log n)
-#include <iostream>
+// Method 1
+// #include<iostream>
+// using namespace std;
+// int pow(int m, int n){
+//     if(n==0){
+//         return 1;
+//     }else{
+//         return pow(m,n-1)*m;
+//     }
+// }
+// int main(){
+//     int p=pow(2,5);
+//     cout<<p;
+//     return 0;
+// }
+
+// Method 2
+#include<iostream>
 using namespace std;
-
-class Solution {
-public:
-    double myPow(double x, int n) {
-        if (n == 0) return 1.0;
-        if (x == 0) return 0.0;
-        if (x == 1) return 1.0;
-        if (x == -1 && n % 2 == 0) return 1.0;
-        if (x == -1 && n % 2 != 0) return -1.0;
-
-        long long binaryForm = n; // use long long to handle INT_MIN safely
-
-        if (n < 0) {
-            x = 1 / x;
-            binaryForm = -binaryForm;
-        }
-
-        double ans = 1.0;
-        while (binaryForm > 0) {
-            if (binaryForm % 2 == 1) {
-                ans *= x;
-            }
-            x *= x;
-            binaryForm /= 2;
-        }
-        return ans;
+int pow(int m, int n){
+    if(n==0){
+        return 1;
+    }else if(n%2==0){
+        return pow(m*m,n/2);
+    }else{
+        return m*pow(m*m,(n-1)/2);
     }
-};
-
-int main() {
-    Solution sol;
-
-    cout << sol.myPow(2.0, 10) << endl;   // 1024
-    cout << sol.myPow(2.0, -2) << endl;   // 0.25
-    cout << sol.myPow(3.0, 0) << endl;    // 1
-    cout << sol.myPow(-1.0, 3) << endl;   // -1
-    cout << sol.myPow(-1.0, 4) << endl;   // 1
-
+}
+int main(){
+    int p=pow(2,5);
+    cout<<p;
     return 0;
 }
